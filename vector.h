36a6693f7100a47e4ab5e66e87624c2d46909c4e
daryadaryandria <knemune@gmail.com>
2025-09-06 20:54:33 +0300

    Vector
    Manual memory management with guarantee of safety

--- vector.h
+++ vector.h
@@ -1,5 +1,6 @@
 #ifndef VECTOR_H
 #define VECTOR_H
+#define VECTOR_MEMORY_IMPLEMENTED
 
 #include <initializer_list>
 #include <algorithm>
@@ -30,17 +31,26 @@ public:
 
   explicit Vector(const SizeType size) : size_(size), capacity_(size) {
     if (capacity_ > 0) {
-      array_ = new ValueType[capacity_]{};
+      array_ = static_cast<T *>(::operator new(size_ * sizeof(T)));
+      try {
+        std::uninitialized_value_construct_n(array_, size_);
+      } catch (...) {
+        ::operator delete(array_);
+        array_ = nullptr;
+        size_ = 0;
+        capacity_ = 0;
+        throw;
+      }
     }
   }
 
-  explicit Vector(const SizeType size, ConstReference value) : size_(size), capacity_(size) {
-    if (capacity_ > 0) {
-      array_ = new ValueType[capacity_];
+  explicit Vector(SizeType size, ConstReference value) : size_(size), capacity_(size) {
+    if (size > 0) {
+      array_ = static_cast<T *>(::operator new(size_ * sizeof(T)));
       try {
-        std::fill(array_, array_ + size_, value);
+        std::uninitialized_fill_n(array_, size, value);
       } catch (...) {
-        delete[] array_;
+        ::operator delete(array_);
         array_ = nullptr;
         size_ = 0;
         capacity_ = 0;
@@ -52,72 +62,92 @@ public:
   template<class Iterator, class = std::enable_if_t<std::is_base_of_v<std::forward_iterator_tag,
     typename std::iterator_traits<Iterator>::iterator_category> > >
   explicit Vector(Iterator first, Iterator last) : size_(std::distance(first, last)), capacity_(size_) {
-    if (capacity_ > 0 && first != last) {
-      array_ = new ValueType[capacity_];
-      try {
-        std::copy(first, last, array_);
-      } catch (...) {
-        delete[] array_;
-        array_ = nullptr;
-        size_ = 0;
-        capacity_ = 0;
+    try {
+      if (size_ > 0) {
+        array_ = static_cast<T *>(::operator new(size_ * sizeof(T)));
+        std::uninitialized_copy(first, last, array_);
       }
+    } catch (...) {
+      ::operator delete(array_);
+      array_ = nullptr;
+      size_ = 0;
+      capacity_ = 0;
+      throw;
     }
   }
 
-  Vector(std::initializer_list<ValueType> init) : size_(init.size()), capacity_(size_) {
-    if (capacity_ > 0) {
-      array_ = new ValueType[capacity_];
+  Vector(std::initializer_list<ValueType> init) : size_(init.size()), capacity_(size_), array_(nullptr) {
+    if (size_ != 0 && init.begin() != init.end()) {
+      array_ = static_cast<T *>(::operator new(size_ * sizeof(T)));
       try {
-        std::copy(init.begin(), init.end(), array_);
+        std::uninitialized_copy(init.begin(), init.end(), array_);
       } catch (...) {
-        delete[] array_;
+        ::operator delete(array_);
         array_ = nullptr;
         size_ = 0;
         capacity_ = 0;
+        throw;
       }
     }
   }
 
   Vector(const Vector &other) : size_(other.size_), capacity_(other.capacity_) {
     if (capacity_ > 0) {
-      array_ = new ValueType[capacity_];
+      array_ = static_cast<T *>(::operator new(size_ * sizeof(T)));
       try {
-        std::copy(other.array_, other.array_ + size_, array_);
+        std::uninitialized_copy(other.begin(), other.end(), array_);
       } catch (...) {
-        delete[] array_;
+        ::operator delete(array_);
         array_ = nullptr;
         size_ = 0;
         capacity_ = 0;
+        throw;
       }
     }
   }
 
-  Vector(Vector &&other) noexcept : size_(std::exchange(other.size_, 0)),
-                                    capacity_(std::exchange(other.capacity_, 0)),
-                                    array_(std::exchange(other.array_, nullptr)) {
+  Vector(Vector &&other) noexcept {
+    array_ = other.array_;
+    size_ = other.size_;
+    capacity_ = other.capacity_;
+    other.size_ = 0;
+    other.capacity_ = 0;
+    other.array_ = nullptr;
   }
 
   Vector &operator=(const Vector &other) {
     if (this != &other) {
-      Vector temp(other);
-      Swap(temp);
+      if (other.capacity_ == 0) {
+        std::destroy_n(array_, size_);
+        ::operator delete(array_);
+        array_ = nullptr;
+        capacity_ = 0;
+        size_ = 0;
+      } else {
+        Vector temp(other);
+        Swap(temp);
+      }
     }
     return *this;
   }
 
   Vector &operator=(Vector &&other) noexcept {
     if (this != &other) {
-      delete[] array_;
-      size_ = std::exchange(other.size_, 0);
-      capacity_ = std::exchange(other.capacity_, 0);
-      array_ = std::exchange(other.array_, nullptr);
+      std::destroy_n(array_, size_);
+      ::operator delete(array_);
+      array_ = other.array_;
+      size_ = other.size_;
+      capacity_ = other.capacity_;
+      other.size_ = 0;
+      other.capacity_ = 0;
+      other.array_ = nullptr;
     }
     return *this;
   }
 
   ~Vector() {
-    delete[] array_;
+    Clear();
+    ::operator delete(array_);
   }
 
   [[nodiscard]] SizeType Size() const { return size_; }
@@ -167,74 +197,88 @@ public:
   }
 
   void Clear() noexcept {
-    delete[] array_;
-    if (capacity_ > 0) {
-      array_ = new ValueType[capacity_];
-    }
-    else {
-      array_ = nullptr;
-    }
+    std::destroy(array_, array_ + size_);
     size_ = 0;
   }
 
   void Resize(const SizeType new_size) {
-    if (new_size <= capacity_) {
+    if (new_size == 0) {
+      std::destroy_n(array_, size_);
+      size_ = 0;
+      return;
+    }
+    if (new_size <= size_) {
+      std::destroy(array_ + new_size, array_ + size_);
       size_ = new_size;
-    } else {
-      Pointer new_array = nullptr;
-      if (new_size > 0) {
-        try {
-          new_array = new ValueType[new_size];
-          std::move(array_, array_ + size_, new_array);
-          for (SizeType i = size_; i < new_size; ++i) {
-            new_array[i] = ValueType();
-          }
-          delete[] array_;
-          array_ = new_array;
-          size_ = new_size;
-          capacity_ = new_size;
-        } catch (...) {
-          delete[] new_array;
-          throw;
+      return;
+    }
+    if (new_size > size_ && new_size <= capacity_) {
+      std::uninitialized_value_construct_n(array_ + size_, new_size - size_);
+      size_ = new_size;
+      return;
+    }
+    Pointer new_array = nullptr;
+    SizeType constructed = 0;
+    if (new_size > 0) {
+      try {
+        new_array = static_cast<T *>(::operator new(new_size * sizeof(T)));
+        for (; constructed < size_; ++constructed) {
+          new (new_array + constructed) T(std::move_if_noexcept(array_[constructed]));
         }
-      } else {
-        delete[] array_;
-        array_ = nullptr;
-        size_ = 0;
+        std::uninitialized_value_construct_n(new_array + size_, new_size - size_);
+        std::destroy(array_, array_ + size_);
+        ::operator delete(array_);
+        array_ = new_array;
+        size_ = new_size;
+        capacity_ = new_size;
+      } catch (...) {
+        std::destroy_n(new_array, constructed);
+        ::operator delete(new_array);
+        throw;
       }
     }
   }
 
   void Resize(const SizeType new_size, ConstReference value) {
+    if (new_size == 0) {
+      std::destroy_n(array_, size_);
+      size_ = 0;
+      return;
+    }
     if (new_size <= size_) {
+      std::destroy(array_ + new_size, array_ + size_);
       size_ = new_size;
-    } else if (new_size > capacity_) {
+    } else if (new_size > size_ && new_size <= capacity_) {
+      std::uninitialized_fill_n(array_ + size_, new_size - size_, value);
+      size_ = new_size;
+    } else {
       Pointer new_array = nullptr;
-      try {
-        if (new_size > 0) {
-          new_array = new ValueType[new_size];
-          std::move(array_, array_ + size_, new_array);
-          for (SizeType i = size_; i < new_size; ++i) {
-            new_array[i] = value;
+      SizeType constructed = 0;
+      if (new_size > 0) {
+        try {
+          new_array = static_cast<T *>(::operator new(new_size * sizeof(T)));
+          for (; constructed < size_; ++constructed) {
+            new (new_array + constructed) T(std::move_if_noexcept(array_[constructed]));
           }
-          delete[] array_;
+          std::uninitialized_fill_n(new_array + size_, new_size - size_, value);
+          std::destroy(array_, array_ + size_);
+          ::operator delete(array_);
           array_ = new_array;
           size_ = new_size;
           capacity_ = new_size;
-        } else {
-          delete[] array_;
-          array_ = nullptr;
-          size_ = new_size;
+        } catch (...) {
+          std::destroy_n(new_array, constructed);
+          ::operator delete(new_array);
+          throw;
         }
-      } catch (...) {
-        delete[] new_array;
-        throw;
+      } else {
+        Clear();
+        std::destroy(array_, array_ + size_);
+        ::operator delete(array_);
+        array_ = nullptr;
+        size_ = 0;
+        capacity_ = 0;
       }
-    } else {
-      for (SizeType i = size_; i < new_size; ++i) {
-        array_[i] = value;
-      }
-      size_ = new_size;
     }
   }
 
@@ -243,17 +287,20 @@ public:
     if (new_capacity <= capacity_) {
       return;
     }
-    ValueType *new_array = nullptr;
+    Pointer new_array = nullptr;
+    SizeType constructed = 0;
     try {
-      new_array = new ValueType[new_capacity];
-      for (SizeType i = 0; i < size_; ++i) {
-        new_array[i] = std::move(array_[i]);
+      new_array = static_cast<T *>(::operator new(new_capacity * sizeof(T)));
+      for (; constructed < size_; ++constructed) {
+        new (new_array + constructed) T(std::move_if_noexcept(array_[constructed]));
       }
-      delete[] array_;
-      array_ = new_array;
       capacity_ = new_capacity;
+      std::destroy(array_, array_ + size_);
+      ::operator delete(array_);
+      array_ = new_array;
     } catch (...) {
-      delete[] new_array;
+      std::destroy(new_array, new_array + size_);
+      ::operator delete(new_array);
       throw;
     }
   }
@@ -261,78 +308,87 @@ public:
 
   void ShrinkToFit() {
     if (size_ == 0) {
-      delete[] array_;
+      ::operator delete(array_);
       array_ = nullptr;
+      size_ = 0;
       capacity_ = 0;
       return;
     }
-    ValueType *new_array = nullptr;
+    Pointer new_array = nullptr;
+    SizeType constructed = 0;
     try {
-      new_array = new ValueType[size_];
-      for (SizeType i = 0; i < size_; ++i) {
-        new_array[i] = std::move(array_[i]);
+      new_array = static_cast<T *>(::operator new(size_ * sizeof(T)));
+      for (; constructed < size_; ++constructed) {
+        new (new_array + constructed) T(std::move_if_noexcept(array_[constructed]));
       }
-
-      delete[] array_;
+      std::destroy(array_, array_ + size_);
+      ::operator delete(array_);
       array_ = new_array;
       capacity_ = size_;
     } catch (...) {
-      delete[] new_array;
+      std::destroy(new_array, new_array + constructed);
+      ::operator delete(new_array);
       throw;
     }
   }
 
   void PushBack(ConstReference value) {
     if (size_ < capacity_) {
-      array_[size_] = value;
+      std::construct_at(array_ + size_, value);
       ++size_;
       return;
     }
     Pointer new_array = nullptr;
+    SizeType constructed = 0;
     try {
       const SizeType cap = capacity_ == 0 ? 1 : capacity_ * 2;
-      new_array = new ValueType[cap];
-      for (SizeType i = 0; i < size_; ++i) {
-        new_array[i] = std::move(array_[i]);
+      new_array = static_cast<T *>(::operator new(cap * sizeof(T)));
+      for (; constructed < size_; ++constructed) {
+        new (new_array + constructed) T(std::move(array_[constructed]));
       }
-      new_array[size_] = value;
-      delete[] array_;
+      std::construct_at(new_array + size_, value);
+      std::destroy(array_, array_ + size_);
+      ::operator delete(array_);
       array_ = new_array;
       ++size_;
       capacity_ = cap;
     } catch (...) {
-      delete[] new_array;
+      std::destroy(new_array, new_array + constructed);
+      ::operator delete(new_array);
       throw;
     }
   }
 
   void PushBack(T &&value) {
     if (size_ < capacity_) {
-      array_[size_] = std::move(value);
+      std::construct_at(array_ + size_, std::move(value));
       ++size_;
       return;
     }
     Pointer new_array = nullptr;
+    SizeType constructed = 0;
     try {
       const SizeType cap = capacity_ == 0 ? 1 : capacity_ * 2;
-      new_array = new ValueType[cap];
-      for (SizeType i = 0; i < size_; ++i) {
-        new_array[i] = std::move(array_[i]);
+      new_array = static_cast<T *>(::operator new(cap * sizeof(T)));
+      for (; constructed < size_; ++constructed) {
+        new (new_array + constructed) T(std::move(array_[constructed]));
       }
-      new_array[size_] = std::move(value);
-      delete[] array_;
+      std::construct_at(new_array + size_, std::move(value));
+      std::destroy(array_, array_ + size_);
+      ::operator delete(array_);
       array_ = new_array;
       ++size_;
       capacity_ = cap;
     } catch (...) {
-      delete[] new_array;
+      std::destroy(new_array, new_array + constructed);
+      ::operator delete(new_array);
       throw;
     }
   }
 
   void PopBack() {
     if (size_ > 0) {
-      array_[size_ - 1] = ValueType();
+      std::destroy(array_ + size_ - 1, array_ + size_);
       --size_;
     }
   }
@@ -375,52 +431,80 @@ public:
     return other <= *this;
   }
 
-  Iterator begin() {//NOLINT
+  Iterator begin() {    //NOLINT
     return array_;
   }
 
-  ConstIterator begin() const {//NOLINT
+  ConstIterator begin() const {    //NOLINT
     return array_;
   }
 
-  ConstIterator cbegin() const {//NOLINT
+  ConstIterator cbegin() const {    //NOLINT
     return array_;
   }
 
-  Iterator end() {//NOLINT
+  Iterator end() {    //NOLINT
     return array_ + size_;
   }
 
-  ConstIterator end() const {//NOLINT
+  ConstIterator end() const {    //NOLINT
     return array_ + size_;
   }
 
-  ConstIterator cend() const {//NOLINT
+  ConstIterator cend() const {    //NOLINT
     return array_ + size_;
   }
 
-  ReverseIterator rbegin() {//NOLINT
+  ReverseIterator rbegin() {    //NOLINT
     return ReverseIterator(end());
   }
 
-  ReverseIterator rend() {//NOLINT
+  ReverseIterator rend() {    //NOLINT
     return ReverseIterator(begin());
   }
 
-  ConstReverseIterator crbegin() const {//NOLINT
+  ConstReverseIterator crbegin() const {    //NOLINT
     return ConstReverseIterator(end());
   }
 
-  ConstReverseIterator crend() const {//NOLINT
+  ConstReverseIterator crend() const {    //NOLINT
     return ConstReverseIterator(begin());
   }
 
-  ConstReverseIterator rbegin() const {//NOLINT
+  ConstReverseIterator rbegin() const {    //NOLINT
     return ConstReverseIterator(end());
   }
 
-  ConstReverseIterator rend() const {//NOLINT
+  ConstReverseIterator rend() const {    //NOLINT
     return ConstReverseIterator(begin());
+  }
+
+  template<typename... Args>
+  void EmplaceBack(Args &&... args) {
+    if (size_ < capacity_) {
+      new(array_ + size_) T(std::forward<Args>(args)...);
+      ++size_;
+      return;
+    }
+    Pointer new_array = nullptr;
+    SizeType constructed = 0;
+    try {
+      const SizeType cap = capacity_ == 0 ? 1 : capacity_ * 2;
+      new_array = static_cast<T *>(::operator new(cap * sizeof(T)));
+      for (; constructed < size_; ++constructed) {
+        new (new_array + constructed) T(std::move_if_noexcept(array_[constructed]));
+      }
+      std::destroy(array_, array_ + size_);
+      ::operator delete(array_);
+      array_ = new_array;
+      capacity_ = cap;
+    } catch (...) {
+      std::destroy(new_array, new_array + size_);
+      ::operator delete(new_array);
+      throw;
+    }
+    new(array_ + size_) T(std::forward<Args>(args)...);
+    ++size_;
   }
 };
 
